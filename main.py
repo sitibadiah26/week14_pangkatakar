7a9a0fbebdf97c81ef2cb96cfa48af7242965737
Siti Badi'ah <60890713+sitibadiah26@users.noreply.github.com>
2020-05-06 21:08:42 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from lib.pangkat import Pangkat
+from lib.akar import Akar
+
+if __name__=="__main__":
+
+    nilai1 = [-1,5,7]
+    nilai2 = [81,9,0]
+
+    print("MENGHITUNG PERPANGKATAN DAN PERAKARAN\n")
+
+    for i in range(len(nilai1)):
+
+        pangkat=Pangkat("Pangkat")
+        pangkat.hitung(nilai1[i])
+        print(pangkat)
+        pangkat.hasil(i)
+
+        akar=Akar("Akar")
+        akar.hitung(nilai2[i])
+        print(akar)
+        akar.hasil(i)
+        
+        print("\n")
+                   
